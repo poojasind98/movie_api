3a132c53b1933b2c56c6a8f35422f83c051684f7
poojasind98 <99157169+poojasind98@users.noreply.github.com>
2022-05-20 08:24:56 +0530

    Add files via upload

---
+++ movie_api/movies/serializers.py
@@ -0,0 +1,9 @@
+from unittest.util import _MAX_LENGTH
+from rest_framework import serializers
+from .models import Moviedata
+
+class MovieSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None,use_url=True)
+    class Meta:
+        model = Moviedata
+        fields =['id','name','duration','rating','genre','image']
